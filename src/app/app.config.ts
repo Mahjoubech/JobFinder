9cf99ed866d573ed49e06c25d6328e640ba03408
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-09 22:18:15 +0100

    add API interceptor to include user ID in request headers; update app configuration to use the interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,9 +5,12 @@ import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {apiInterceptor} from './core/interceptors/api-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([apiInterceptor])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideStore(),
--- src/app/core/interceptors/api-interceptor.ts
+++ src/app/core/interceptors/api-interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const user = localStorage.getItem('user');
+  if(user){
+    const parsedUser = JSON.parse(user);
+    const cloned = req.clone({
+      setHeaders: {
+        'user_id' : parsedUser.id
+      }
+    })
+    return next(cloned);
+  }
   return next(req);
 };

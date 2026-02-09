bbc57102e0797dfebf18b7d0992896b68da05203
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-09 16:25:38 +0100

    add NgRx store, effects, and devtools integration; update user data in db.json

---
+++ src/app/features/applications/applications-list/applications-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applications-list',
+  imports: [],
+  templateUrl: './applications-list.html',
+  styleUrl: './applications-list.css',
+})
+export class ApplicationsList {
+
+}

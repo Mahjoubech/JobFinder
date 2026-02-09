03240e8e96dbd06d1d043de952bd8e72b27fd71d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-09 21:45:02 +0100

    add routing for authentication and job listing; implement AuthGuard for protected routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {Register} from './features/auth/register/register';
+import {JobList} from './features/jobs/job-list/job-list';
+import {Login} from './features/auth/login/login';
+import {Profile} from './features/auth/profile/profile';
+import {AuthGuard} from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : "register" , component: Register},
+  {path: "dashboard" , component: JobList},
+  {path:'login' , component: Login},
+  {path:'profile' , component:Profile , canActivate:[AuthGuard]}
+];

677af5d5163f3a2d995172d443bacfae41c7c0b0
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-09 23:33:08 +0100

    add login component with form handling and error management; implement authentication logic and navigation

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Auth} from '../../../core/service/auth';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule , CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  email:string = "";
+  password:string = "";
+  errorMess:string | null = null;
+  constructor(private authService : Auth , private router:Router) {}
+  login(){
+    this.errorMess = null;
+    this.authService.login(this.email , this.password).subscribe(
+      result => {
+        if('error' in result){
+          this.errorMess = result.error;
+        }else{
+          localStorage.setItem('user' , JSON.stringify(result));
+          this.router.navigate(['/dashboard']);
+        }
+      }
+    )
 
+  }
 }

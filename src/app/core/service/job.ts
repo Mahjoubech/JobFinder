9fd4bdef914c96765d0ca22a36ab902b7cd9460d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 16:07:13 +0100

    add job search functionality; implement keyword and location filtering, enhance loading state management, and optimize job retrieval logic

--- src/app/core/service/job.ts
+++ src/app/core/service/job.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import {BehaviorSubject, finalize, map, Observable} from 'rxjs';
 export interface Job{
   id:number;
   name: string;
@@ -16,11 +16,45 @@ export interface Job{
   providedIn: 'root',
 })
 export class JobService  {
-  private API_URL = '/themuse-api/api/public/jobs?page=0&descending=true';
+  private API_URL = '/themuse-api/api/public/jobs';
+  private jobSubject = new BehaviorSubject<Job[]>([])
+  jobs$= this.jobSubject.asObservable();
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
   constructor(private http: HttpClient) {}
   getJobs(page:number = 0 , limit:number = 20): Observable<Job[]> {
-    const url = `/themuse-api/api/public/jobs?page=${page}&items_per_page=${limit}&descending=true`;
+    const url = `${this.API_URL}?page=${page}&items_per_page=${limit}&descending=true`;
     return this.http.get<any>(url).pipe(
       map(res => res.results.slice(0 , limit))
     );
-  }}
+  }
+  srearch(keyword : string , location : string , page : number = 0) : void{
+      this.loadingSubject.next(true);
+    let url = `${this.API_URL}?page=${page}&descending=true`;
+    if(location){
+      url += `&location=${encodeURIComponent(location)}`;
+    }
+
+    this.http.get<any>(url).pipe(
+      map(res =>{
+        let result = res.results;
+        if (keyword){
+          const lowerKey = keyword.toLowerCase();
+          result = result.filter((job : Job) => {
+            job.name.toLowerCase() === lowerKey
+          });
+        }
+        return result.slice(0 , 8)
+      }),
+      finalize(()=>{this.loadingSubject.next(false)})
+    ).subscribe(jobs => {
+      if(page === 0){
+        this.jobSubject.next(jobs);
+      }else{
+        const cuurent = this.jobSubject.value;
+        this.jobSubject.next([...cuurent , ...jobs]);
+      }
+    })
+  }
+
+}

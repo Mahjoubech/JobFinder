fac4f560f6f3d072b4c8ded8cda3b71bc35f7861
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:23:05 +0100

    add job list component; implement infinite scrolling and loading state for job listings

--- src/app/features/jobs/job-list/job-list.ts
+++ src/app/features/jobs/job-list/job-list.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { JobCard } from '../job-card/job-card';
+import {CommonModule} from '@angular/common';
+import {Job, JobService} from '../../../core/service/job';
+import {Loader} from '../../../shared/components/loader/loader';
 
 @Component({
   selector: 'app-job-list',
-  imports: [],
+  standalone:true,
+  imports: [JobCard , CommonModule ,Loader],
   templateUrl: './job-list.html',
   styleUrl: './job-list.css',
 })
-export class JobList {
+export class JobList implements OnInit{
+jobs:Job[] = [];
+page = 0;
+limit = 8 ;
+loading = false ;
+allLoaded = false ;
+constructor(protected jobService : JobService) {}
+  ngOnInit(): void {
+  this.loadJobs();
+  }
+  loadJobs(){
+  if (this.loading || this.allLoaded) return;
+    this.loading = true;
+  this.jobService.getJobs(this.page , this.limit).subscribe({
+    next : (newJobs)=> {
+      if(newJobs.length === 0){
+        this.allLoaded = true;
+      }else{
+        this.jobs  = [...this.jobs , ...newJobs ]
+        this.page++;
+      }
+      this.loading = false;
+      console.log(`Loaded page ${this.page}, total jobs: ${this.jobs.length}`);
+      },
+    error:(err)=> {console.error('Error fetching Jobs ' , err);
+      this.loading = false;
+    }
 
+
+  })
+    }
+  onScroll(event: any) {
+    const element = event.target;
+    if (element.scrollHeight - element.scrollTop <= element.clientHeight + 50) {
+      this.loadJobs();
+    }
+  }
+  trackByJob(index: number, job: Job): number {
+    return job.id;
+  }
 }

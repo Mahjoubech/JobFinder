b7d6560deefefa37a27612023781f27269f001c0
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:22:12 +0100

    add profile menu toggle; implement functionality to show/hide profile menu in navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink , CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  showProfileMenu = false;
 
+  toggleProfileMenu() {
+    this.showProfileMenu = !this.showProfileMenu;
+  }
 }

7231c6d372f2d5f89dfdbd9dc1fae6327297766a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:22:02 +0100

    add header component; implement header UI and styling for job search page

--- src/app/features/jobs/job-search/job-search.ts
+++ src/app/features/jobs/job-search/job-search.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import {Navbar} from '../../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-job-search',
-  imports: [],
+  imports: [RouterLink, CommonModule ,Navbar],
   templateUrl: './job-search.html',
   styleUrl: './job-search.css',
 })

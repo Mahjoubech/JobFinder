e2f55ce797a4c2a299d6d3ca358222fcb5f708df
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:22:41 +0100

    refactor job card; implement dynamic company logo retrieval and optimize change detection

--- src/app/features/jobs/job-card/job-card.ts
+++ src/app/features/jobs/job-card/job-card.ts
@@ -1,14 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input ,ChangeDetectionStrategy } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Job} from '../../../core/service/job';
 
 @Component({
   selector: 'app-job-card',
   standalone:true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
   templateUrl: './job-card.html',
   styleUrl: './job-card.css',
 })
 export class JobCard {
  @Input() job!: Job;
+
+  getCompanyLogo(name: string): string {
+    if (!name) return 'https://via.placeholder.com/40';
+    // Remove spaces, commas, "Inc", "LLC" and convert to lowercase
+    const cleanName = name
+      .replace(/[,.]/g, '') // Remove dots and commas
+      .replace(/\s/g, '')   // Remove spaces
+      .replace(/Inc$/i, '')
+      .replace(/LLC$/i, '');
+
+    return `https://logo.clearbit.com/${cleanName}.com`;
+  }
 }

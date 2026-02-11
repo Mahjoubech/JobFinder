c134fc98cab224a1f4159b948c5ad598ce69b6b4
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:20:59 +0100

    add job detail component; implement job detail UI and styling

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { JobSearch } from '../jobs/job-search/job-search';
+import { JobFilter } from '../jobs/job-filter/job-filter';
+import { JobList } from '../jobs/job-list/job-list';
+import { JobDetail } from '../jobs/job-detail/job-detail';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ JobSearch, JobFilter, JobList, JobDetail],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

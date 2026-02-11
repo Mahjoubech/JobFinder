f323346ef1df294225f49e8db52e9195beb18859
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 05:20:38 +0100

    add job filter component; implement filter UI and styling for job listings

---
+++ src/app/features/jobs/job-filter/job-filter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-filter',
+  imports: [],
+  templateUrl: './job-filter.html',
+  styleUrl: './job-filter.css',
+})
+export class JobFilter {
+
+}

f6d0ed2cf1f9ae6a8bc452ec912e79b20571eb08
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-11 03:53:18 +0100

    add job service; implement API integration for fetching job listings; define job data structure

--- src/app/core/service/job.ts
+++ src/app/core/service/job.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+export interface Job{
+  id:number;
+  name: string;
+  company : {
+    name:string;
+    id:number;
+  }
+  locations: {name: string}[];
+  levels: {name:string}[];
+  publication_date:string;
+}
 @Injectable({
   providedIn: 'root',
 })
-export class Job {
-  
+export class JobService  {
+  private API_URL = 'https://www.themuse.com/api/public/jobs?page=0&descending=true';
+  constructor(private http: HttpClient) {}
+  getJobs(): Observable<Job[]>{
+    return this.http.get<any>(this.API_URL).pipe(
+      map(res => res.results)
+    )
+  }
 }

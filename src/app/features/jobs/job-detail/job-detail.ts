df61d9c529d53d506b9eec01ee2a7aa28584e9c2
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-12 19:08:37 +0100

    refactor job detail component; implement dynamic job data binding, enhance company logo display, and improve job description handling

--- src/app/features/jobs/job-detail/job-detail.ts
+++ src/app/features/jobs/job-detail/job-detail.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
-
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Job, JobService} from '../../../core/service/job';
+import {CommonModule} from '@angular/common';
+import {LogoBackgroundPipe} from '../../../shared/pipes/logo-background-pipe';
+import {CompanyInitialsPipe} from '../../../shared/pipes/company-initials-pipe';
 @Component({
   selector: 'app-job-detail',
-  imports: [],
+  standalone:true,
+  changeDetection: ChangeDetectionStrategy.Default,
+  imports: [CommonModule ,  LogoBackgroundPipe , CompanyInitialsPipe],
   templateUrl: './job-detail.html',
   styleUrl: './job-detail.css',
 })
-export class JobDetail {
+export class JobDetail implements OnInit{
   isFavorite = false;
+ job:Job | null = null;
+ constructor(private jobService : JobService) {}
 
+  ngOnInit(): void {
+        this.jobService.selectedJob$.subscribe(
+          job => {
+            console.log(job);
+            this.job = job;
+          }
+        )
+    }
+  get companyDescriptionSnippet(): string {
+    if (!this.job?.contents) return '';
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = this.job.contents;
+    const text = tempDiv.textContent || tempDiv.innerText || '';
+    return text.length > 200 ? text.substring(0, 200) + '...' : text;
+  }
   toggleFavorite() {
     this.isFavorite = !this.isFavorite;
   }

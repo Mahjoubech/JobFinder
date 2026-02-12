75d61fee47dd5ec90ae922da2ef932b51dd3885c
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-12 04:35:33 +0100

    add search functionality; implement keyword search and reset features in job search component, enhance input handling, and update styles for better dark mode support

--- src/app/features/jobs/job-search/job-search.ts
+++ src/app/features/jobs/job-search/job-search.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import {Navbar} from '../../../shared/components/navbar/navbar';
+import {FormsModule} from '@angular/forms';
+import {JobService} from '../../../core/service/job';
 
 @Component({
   selector: 'app-job-search',
-  imports: [RouterLink, CommonModule ,Navbar],
+  imports: [RouterLink, CommonModule ,Navbar , FormsModule],
   templateUrl: './job-search.html',
   styleUrl: './job-search.css',
 })
 export class JobSearch {
+  keyword = "";
+  constructor(private  jobService : JobService) {}
+  onSearch(){
+    this.jobService.search(this.keyword);
+  }
 
+  onReset() {
+    this.keyword = '';
+    this.jobService.resetSearch();
+  }
 }

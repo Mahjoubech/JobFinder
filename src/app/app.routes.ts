50d5de3e1ebae39ae37f5378c408654be78fcf1a
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-14 00:57:57 +0100

    add routing for My Jobs and Job Apply components; implement redirects and authentication guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,19 @@ import {Login} from './features/auth/login/login';
 import {Profile} from './features/auth/profile/profile';
 import {AuthGuard} from './core/guards/auth-guard';
 import {Home} from './features/home/home';
+import {ApplicationsList} from './features/applications/applications-list/applications-list';
+import {MyJobs} from './features/my-jobs/my-jobs';
+
+import {JobApply} from './features/jobs/job-apply/job-apply';
 
 export const routes: Routes = [
   {path : "register" , component: Register},
   {path: "" , component: Home},
   {path:'login' , component: Login},
-  {path:'profile' , component:Profile }
+  {path:'profile' , component:Profile, canActivate: [AuthGuard] },
+  {path: 'my-jobs', redirectTo: 'my-jobs/saved', pathMatch: 'full'},
+  {path: 'my-jobs/:tab', component: MyJobs, canActivate: [AuthGuard]},
+  {path: 'favorites', redirectTo: 'my-jobs/saved', pathMatch: 'full'},
+  {path: 'applications', redirectTo: 'my-jobs/applied', pathMatch: 'full'},
+  {path: 'jobs/:id/apply', component: JobApply, canActivate: [AuthGuard]}
 ];

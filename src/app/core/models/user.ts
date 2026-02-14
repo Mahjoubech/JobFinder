19db418decbf0d77f92ebf4c1b149fd79615dacf
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-14 05:50:55 +0100

    add user interface; define User model with properties for personal and profile information

---
+++ src/app/core/models/user.ts
@@ -0,0 +1,10 @@
+export interface User{
+  id: string,
+  firstName:string,
+  lastName:string,
+  email:string,
+  password:string,
+  resumeUrl?: string,
+  jobTitle?: string,
+  profileImage?: string
+}

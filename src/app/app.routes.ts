e7432d3735425ac5f2b414efcf7404eeb177567d
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-02-14 19:06:07 +0100

    refactor routing to use loadComponent for lazy loading of features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,38 @@
 import { Routes } from '@angular/router';
-import {Register} from './features/auth/register/register';
-import {JobList} from './features/jobs/job-list/job-list';
-import {Login} from './features/auth/login/login';
-import {Profile} from './features/auth/profile/profile';
 import {AuthGuard} from './core/guards/auth-guard';
 import {Home} from './features/home/home';
-import {ApplicationsList} from './features/applications/applications-list/applications-list';
-import {MyJobs} from './features/my-jobs/my-jobs';
-
-import {JobApply} from './features/jobs/job-apply/job-apply';
 import { NotFound } from './features/not-found';
 
 export const routes: Routes = [
-  {path : "register" , component: Register},
-  {path: "" , component: Home},
-  {path:'login' , component: Login},
-  {path:'profile' , component:Profile, canActivate: [AuthGuard] },
+  {
+    path : "register", 
+    loadComponent: () => import('./features/auth/register/register').then(m => m.Register)
+  },
+  {
+    path: "", 
+    component: Home
+  },
+  {
+    path:'login', 
+    loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
+  },
+  {
+    path:'profile', 
+    loadComponent: () => import('./features/auth/profile/profile').then(m => m.Profile),
+    canActivate: [AuthGuard] 
+  },
   {path: 'my-jobs', redirectTo: 'my-jobs/saved', pathMatch: 'full'},
-  {path: 'my-jobs/:tab', component: MyJobs, canActivate: [AuthGuard]},
+  {
+    path: 'my-jobs/:tab', 
+    loadComponent: () => import('./features/my-jobs/my-jobs').then(m => m.MyJobs),
+    canActivate: [AuthGuard]
+  },
   {path: 'favorites', redirectTo: 'my-jobs/saved', pathMatch: 'full'},
   {path: 'applications', redirectTo: 'my-jobs/applied', pathMatch: 'full'},
-  {path: 'jobs/:id/apply', component: JobApply, canActivate: [AuthGuard]},
+  {
+    path: 'jobs/:id/apply', 
+    loadComponent: () => import('./features/jobs/job-apply/job-apply').then(m => m.JobApply),
+    canActivate: [AuthGuard]
+  },
   {path: '**', component: NotFound}
 ];
